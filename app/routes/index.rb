fb9828cc078357582a8e50955e417c1db2fa9671
Avner Cohen <israbirding@gmail.com>
2020-02-27 18:41:35 +0200

    Try to still support the negative numbers

--- app/routes/index.rb
+++ app/routes/index.rb
@@ -17,7 +17,10 @@ module MongoAdmin
 
     delete '/ops/:operation_id' do
       admin_client = @db.client.use('admin')
-      op_id = params['operation_id']
+      op_id = params['operation_id'].to_i
+      if op_id < 0
+        op_id = op_id % 2**32
+      end
       admin_client.command(killOp: 1, op: op_id).first
 
       flash[:info] = I18n.t('op_id_killed', op_id: op_id)
